017d2d49c86cdb355c18423b36ae7c3e8b4b8311
Gavin LaMontagne <Gavin.Lamontagne@outlook.com>
2025-03-09 10:01:17 -0600

    First Commit

---
+++ automobile.java
@@ -0,0 +1,43 @@
+
+public class automobile {
+
+	public static void removeVehicle (String autoMake, String autoModel, String autoColor, int AutoYear) {
+		
+		//FIXME: Create if-else branch
+		System.out.println("FIXME: Finish removeVehicle method.");
+		
+		System.out.println("Mismatch");
+	}
+	
+	public static void addVehicle (String autoMake, String autoModel, String autoColor, int AutoYear) {
+		
+		//FIXME: Create if-else branch
+		System.out.println("FIXME: Finish addVehicle method.");
+		
+		System.out.println("Mismatch");
+	}
+	
+	public static void updateVehicle (String autoMake, String autoModel, String autoColor, int AutoYear) {
+		
+		//FIXME: Create if-else branch
+		System.out.println("FIXME: Finish updateVehicle method.");
+		
+		System.out.println("Mismatch");
+	}
+	
+	public static void printInventory () {
+		
+		//Create loop to print out all inventory from an array.
+		System.out.println("FIXME: Finish printInventory method.");
+		
+		System.out.println("Mismatch");
+	}	
+	
+	public static void main(String[] args) {
+		removeVehicle("make", "model", "color", 9999);
+		addVehicle("make", "model", "color", 9999);
+		updateVehicle("make", "model", "color", 9999);
+		printInventory();
+	}
+
+}
